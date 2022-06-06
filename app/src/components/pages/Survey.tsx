10e4b2413c959f41594aaaa83ebac331c41b6304
Ahnaf Ahmed <nemba246@gmail.com>
2022-06-05 22:46:57 -0700

    added relief stuff, need to add state mangagement

--- app/src/components/pages/Survey.tsx
+++ app/src/components/pages/Survey.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import Stack from '@mui/material/Stack';
 import SurveyClassQuestion from 'components/organisms/SurveyClassQuestion';
 import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import TextField from '@mui/material/TextField';
 
 function Survey() {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -15,11 +18,21 @@ function Survey() {
         <Stack spacing={4}>
           <SurveyClassQuestion name="Seng 310" />
           <SurveyClassQuestion name="Seng 265" />
-        </Stack>
+          <FormControlLabel control={<Checkbox defaultChecked />} label="Have Relief?"/>
+          <TextField
+            id="outlined-textarea"
+            label="Explanation"
+            multiline
+            style = {{width: 500}}
+            inputProps={{ style: { color: 'black' } }}
+          />
 
         <Button variant="contained" color="primary" type="submit">
           Submit
         </Button>
+        </Stack>
+
+        
       </form>
     </div>
   );
