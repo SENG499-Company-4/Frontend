3fca265b50de932887a1fa5d3cdaf309e3cb2fad
Sam Warren <samwarren4@gmail.com>
2022-06-07 10:59:40 -0700

    update header to atomic design

--- app/src/components/organisms/Header.tsx
+++ app/src/components/organisms/Header.tsx
@@ -1,46 +1,22 @@
 import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import HeaderButton from 'components/molecules/HeaderButton';
 
 function Header() {
-  function navTo(pageName: string) {
-    return () => {
-      window.location.href = `${pageName}`;
-    };
-  }
-
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Box display="flex" flexDirection="row" justifyContent="space-between">
-          <Button onClick={navTo('/')} color="inherit">
-            Home
-          </Button>
-          <Button onClick={navTo('/login')} color="inherit">
-            Login
-          </Button>
-          <Button onClick={navTo('/dashboard')} color="inherit">
-            Dashboard
-          </Button>
-          <Button onClick={navTo('/survey')} color="inherit">
-            Survey
-          </Button>
-          <Button onClick={navTo('/schedule')} color="inherit">
-            Schedule
-          </Button>
-          <Button onClick={navTo('/schedule/manage')} color="inherit">
-            Schedule Manage
-          </Button>
-          <Button onClick={navTo('/schedule/timetable')} color="inherit">
-            Schedule Timetable
-          </Button>
-          <Button onClick={navTo('/schedule/generate')} color="inherit">
-            Schedule Generate
-          </Button>
-          <Button onClick={navTo('/professorprofile')} color="inherit">
-            Professor Profile
-          </Button>
+          <HeaderButton label="Home" url="/" />
+          <HeaderButton label="Login" url="/login" />
+          <HeaderButton label="Dashboard" url="/dashboard" />
+          <HeaderButton label="Survey" url="/survey" />
+          <HeaderButton label="Schedule" url="/schedule" />
+          <HeaderButton label="Manage Schedule" url="/schedule/manage"/>
+          <HeaderButton label="Schedule Timetable" url="/schedule/timetable"/>
+          <HeaderButton label="Generate Schedule" url="/schedule/generate"/>
+          <HeaderButton label="Professor Profile" url="/professor-profile"/>
         </Box>
       </AppBar>
     </Box>
