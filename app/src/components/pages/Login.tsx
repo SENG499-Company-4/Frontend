103adc95f9650bfbd353fd18644ccc74c61eb133
Sam Warren <samwarren4@gmail.com>
2022-06-07 17:39:06 -0700

    Update login and register components

--- app/src/components/pages/Login.tsx
+++ app/src/components/pages/Login.tsx
@@ -1,68 +1,69 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, NavigateFunction, useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { Grid } from '@mui/material';
 
 function Login() {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  let navigate: NavigateFunction = useNavigate();
 
-  let navigate = useNavigate();
-  const routeChange = () => {
-    let path = `/register`;
-    navigate(path);
-  }
+  const register: () => void = () => {
+    navigate('/register');
+  };
 
-  const signIn = (e: { preventDefault: () => void; }) => {
-    e.preventDefault()
-  }
+  const signIn = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+  };
 
   return (
-    <Box
-      component="form"
-      sx={{
-        width: 300,
-        height: 300,
-      }}
-      mx='auto'
-      justifyContent='center'
-      noValidate
-      autoComplete="off"
-    >
-      <div className='login'>
-        <Link to='/'>
-          <img style={{ width: 300, marginTop: 60 }} className='login_logo' src='https://web.uvic.ca/~virsf/graphics/UVic%20Logo.png' alt='' />
+    <Box component="form" sx={{ width: 300 }} mx="auto" justifyContent="center" noValidate autoComplete="off">
+      <Grid container spacing={2} className="login">
+        <Link to="/">
+          <img
+            style={{ width: 300, marginTop: 60 }}
+            className="login_logo"
+            src="https://web.uvic.ca/~virsf/graphics/UVic%20Logo.png"
+            alt=""
+          />
         </Link>
-        <div className='login_container'>
-          <h1>Sign In</h1>
-          <form>
-            <h5>UVIC Email</h5>
-            <TextField
-              id="outlined-required"
-              label="Username"
-              style={{ width: '100%' }}
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-            />
-            <h5>Password</h5>
-            <TextField
-              id="outlined-password-input"
-              label="Password"
-              type="password"
-              autoComplete="current-password"
-              style={{ width: '100%' }}
-              value={password}
-              onChange={p => setPassword(p.target.value)}
-            />
-            <Button variant='contained' style={{ width: '100%', marginTop: 15 }} onClick={signIn}>Sign In</Button>
-          </form>
-          <Button variant='outlined' style={{ width: '100%', marginTop: 15 }} onClick={routeChange}>Register</Button>
-        </div>
-      </div >
-    </Box >
-  )
+        <Grid item>
+          <Typography variant="h3">Sign In</Typography>
+        </Grid>
+        <Grid item>
+          <TextField
+            id="outlined-username-input"
+            style={{ width: 300 }}
+            label="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="outlined-password-input"
+            style={{ width: 300 }}
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(p) => setPassword(p.target.value)}
+          />
+        </Grid>
+        <Grid item>
+          <Button variant="contained" style={{ width: 300, marginTop: 15 }} onClick={signIn}>
+            Sign In
+          </Button>
+          <Button variant="outlined" style={{ width: 300, marginTop: 15 }} onClick={register}>
+            Register
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
-export default Login
+export default Login;
--- app/src/components/pages/Register.tsx
+++ app/src/components/pages/Register.tsx
@@ -1,89 +1,107 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
+import { Grid, Typography } from '@mui/material';
 
-function Register() {
-
-    const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [verifyPassword, setVerifyPassword] = useState('');
-    const [verified, setVerified] = useState(0); // 0 is initial, 1 is success, -1 is failure
-
-    const createAccount = (e: { preventDefault: () => void; }) => {
-        e.preventDefault()
-        if (password === verifyPassword) {
-            console.log('Passwords match')
-            setVerified(1)
-        }
-        else {
-            setVerified(-1)
-            setEmail('')
-            setUsername('')
-            setPassword('')
-            setVerifyPassword('')
-        }
-    }
-
-    return (
-        <Box
-            component="form"
-            sx={{
-                width: 300,
-                height: 300,
-            }}
-            m='auto'
-            justifyContent='center'
-            noValidate
-            autoComplete="off"
-        >
-            <div className='register'>
-                <div className='register_container'>
-                    <h1>Create an Account</h1>
-                    {verified === -1 ? <h4>Passwords do not match, try again.</h4> : ''}
-                    <form>
-                        <h5>UVIC Email</h5>
-                        <TextField
-                            id="outlined-required"
-                            label="UVic Email"
-                            style={{ width: '100%' }}
-                            value={email}
-                            onChange={e => setEmail(e.target.value)}
-                        />
-                        <h5>Username</h5>
-                        <TextField
-                            id="outlined-required"
-                            label="Use your UVic ID"
-                            style={{ width: '100%' }}
-                            value={username}
-                            onChange={e => setUsername(e.target.value)}
-                        />
-                        <h5>Password</h5>
-                        <TextField
-                            id="outlined-password-input"
-                            label="Password"
-                            type="password"
-                            autoComplete="current-password"
-                            style={{ width: '100%' }}
-                            value={password}
-                            onChange={p => setPassword(p.target.value)}
-                        />
-                        <h5> Verify Password</h5>
-                        <TextField
-                            id="outlined-password-input"
-                            label="Verify Password"
-                            type="password"
-                            style={{ width: '100%' }}
-                            value={verifyPassword}
-                            onChange={p => setVerifyPassword(p.target.value)}
-                        />
-                        <Button variant="contained" style={{ width: '100%', height: 60, marginTop: 40 }} onClick={createAccount}>Create a Scheduler account</Button>
-                    </form>
-                </div>
-            </div >
-        </Box >
-    )
+interface RegisterForm {
+  email: string;
+  username: string;
+  password: string;
+  confirmPassword: string;
 }
 
-export default Register
+function Register() {
+  const [hasErrors, setHasErrors] = useState<boolean>(false);
+  const [formData, setFormData] = useState<RegisterForm>({
+    email: '',
+    username: '',
+    password: '',
+    confirmPassword: ''
+  });
+
+  const createAccount = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log('Creating account: ', formData);
+  };
+
+  useEffect(() => {
+    formData.password === formData.confirmPassword && formData.password.length > 0
+      ? setHasErrors(false)
+      : setHasErrors(true);
+  }, [formData.password, formData.confirmPassword]);
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        width: 300
+      }}
+      m="auto"
+      justifyContent="center"
+      autoComplete="off"
+    >
+      <Grid container spacing={2} marginTop={2}>
+        <Grid item>
+          <Typography variant="h3">Create an Account</Typography>
+        </Grid>
+        {hasErrors && (
+          <Grid item>
+            <Typography variant="body1" color="error">
+              Passwords do not match.
+            </Typography>
+          </Grid>
+        )}
+        <Grid item>
+          <TextField
+            id="outlined-required"
+            label="UVic Email"
+            placeholder="someone@uvic.ca"
+            style={{ width: 300 }}
+            value={formData.email}
+            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="outlined-required"
+            label="UVic ID"
+            placeholder="V00123456"
+            style={{ width: 300 }}
+            value={formData.username}
+            onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="outlined-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            style={{ width: 300 }}
+            value={formData.password}
+            onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+          />
+        </Grid>
+        <Grid item>
+          <TextField
+            id="outlined-password-input"
+            error={hasErrors}
+            label="Verify Password"
+            type="password"
+            style={{ width: 300 }}
+            value={formData.confirmPassword}
+            onChange={(e) => setFormData({ ...formData, confirmPassword: e.target.value })}
+          />
+        </Grid>
+        <Grid item>
+          <Button variant="contained" disabled={hasErrors} style={{ width: 300 }} onClick={createAccount}>
+            Create a Scheduler account
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default Register;
