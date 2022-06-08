dbbbb98174bc8ab85a586d169f94a9ee1cce6e08
Sam Warren <samwarren4@gmail.com>
2022-06-07 17:43:13 -0700

    linter fix

--- app/src/components/pages/Home.tsx
+++ app/src/components/pages/Home.tsx
@@ -4,28 +4,31 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 
 function Home() {
-
   let navigate = useNavigate();
   const routeChange = () => {
     let path = `/login`;
     navigate(path);
-  }
+  };
 
   return (
-    <Box sx={{
-      display: 'flex',
-      justifyContent: 'center',
-      alignContent: 'center',
-      p: 1,
-      m: 1,
-      bgcolor: 'background.paper',
-      height: 1,
-      width: 1,
-      borderRadius: 1,
-    }}>
-      <Button variant='outlined' style={{ width: '30%', height: 50 }} onClick={routeChange}>Login Now</Button>
-    </Box >
-  )
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignContent: 'center',
+        p: 1,
+        m: 1,
+        bgcolor: 'background.paper',
+        height: 1,
+        width: 1,
+        borderRadius: 1
+      }}
+    >
+      <Button variant="outlined" style={{ width: '30%', height: 50 }} onClick={routeChange}>
+        Login Now
+      </Button>
+    </Box>
+  );
 }
 
 export default Home;
