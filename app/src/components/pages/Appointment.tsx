37db475e25055cae14b7e91cac1ed8d8788e4bef
Sam Warren <samwarren4@gmail.com>
2022-06-09 15:29:28 -0700

    convert js files to tsx files

---
+++ app/src/components/pages/Appointment.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import Query from 'devextreme/data/query';
+import { formatDate } from 'devextreme/localization';
+import classData from 'data/clean.json';
+import { CalendarItem, Course, Faculty } from 'interfaces/interfaces';
+
+/**
+ * Grab data from python scraper and format it for DevExtreme Scheduler
+ *  Reference: https://js.devexpress.com/Demos/WidgetsGallery/Demo/Scheduler/CustomTemplates/React/Light/
+ */
+export function parseCalendarJSON(data: Course[]): CalendarItem[] {
+  return data.map((course: Course) => {
+    const dayInit: string[] = [];
+    course.meetingTimes.forEach((element) => {
+      dayInit.push(element.Day.slice(0, 2));
+    });
+    const calendarItem: CalendarItem = {
+      courseId: (course.CourseID.subject as Faculty) + course.CourseID.code,
+      teacherId: course.professors[0].id, // TODO: Handle multiple professors
+      text: (course.CourseID.subject as Faculty) + course.CourseID.code,
+      startDate: new Date(course.startDate),
+      endDate: new Date(course.endDate),
+      teacherName: course.professors[0].username,
+      recerrenceRule:
+        'FREQ=WEEKLY;BYDAY=' + dayInit.join(',') + ';UNTIL=' + course.endDate.replaceAll('-', '') + 'T000000Z'
+    };
+    return calendarItem;
+  });
+}
+
+const data: CalendarItem[] = parseCalendarJSON(JSON.parse(JSON.stringify(classData)));
+
+/**
+ * Filter newdata by course id
+ */
+
+function getClassById(id: string) {
+  return Query(data).filter(['courseId', id]).toArray()[0];
+}
+
+/**
+ * Present data in following format: coursename(subject & code) + teacher + time
+ */
+function Appointment(model: any) {
+  console.log('MODEL: ', model);
+  const { targetedAppointmentData } = model.data;
+  // console.log(targetedAppointmentData)
+  const classData: CalendarItem = getClassById(targetedAppointmentData.courseId) || {};
+
+  return (
+    <div className="showtime-preview">
+      <div> {classData.text} </div>
+      <div> {classData.teacherName} </div>
+      <div>
+        {formatDate(targetedAppointmentData.displayStartDate, 'shortTime')}
+        {' - '}
+        {formatDate(targetedAppointmentData.displayEndDate, 'shortTime')}
+      </div>
+    </div>
+  );
+}
+
+export default Appointment;
---
+++ app/src/components/pages/ScheduleTimetable.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Scheduler from 'devextreme-react/scheduler';
+import 'devextreme/dist/css/dx.light.css';
+import Appointment, { parseCalendarJSON } from 'components/pages/Appointment';
+import classData from 'data/clean.json';
+
+//The current date will be +1 month in the UI, ex: 2021/Dec/10 -> 2022/Jan/10
+const currentDate = new Date(2021, 12, 10);
+
+function ScheduleTimetable() {
+  const calendarData = parseCalendarJSON(JSON.parse(JSON.stringify(classData)));
+  return (
+    <Scheduler
+      timeZone="Canada/Victoria"
+      dataSource={calendarData}
+      defaultCurrentView="day"
+      defaultCurrentDate={currentDate}
+      startDayHour={8}
+      height={800}
+      appointmentComponent={Appointment}
+      showAllDayPanel={false}
+    />
+  );
+}
+
+export default ScheduleTimetable;
