94ebba18cd7feb0ca6228a3f4a202c5983fb13ce
Sam Warren <samwarren4@gmail.com>
2022-06-11 12:05:37 -0700

    rebase

--- app/src/components/organisms/SurveyForm.tsx
+++ app/src/components/organisms/SurveyForm.tsx
@@ -317,4 +317,3 @@ function SurveyForm(props: { formData: classData[] }) {
 }
 
 export default SurveyForm;
-
--- app/src/components/shared/constants/surveyForm.constants.ts
+++ app/src/components/shared/constants/surveyForm.constants.ts
@@ -20,4 +20,3 @@ export enum willing {
   notWilling = 'notWilling',
   veryWilling = 'veryWilling'
 }
-
