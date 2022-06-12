a4f1a945cabe198c36e6d7ad32d1a4b15ace5a50
Sam Warren <samwarren4@gmail.com>
2022-06-12 16:20:31 -0700

    update error page to display more valuable information

--- app/src/components/pages/ErrorPage.tsx
+++ app/src/components/pages/ErrorPage.tsx
@@ -5,17 +5,29 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/system/Container';
 
-function ErrorPage() {
+interface IErrorPageProps {
+  message?: string;
+  code?: string;
+}
+
+function ErrorPage(props: IErrorPageProps) {
+  const errorTitle = props.code ? 'Error ' + props.code + '' : 'Unknown Error';
+  const errorText = props.message
+    ? props.message
+    : 'The application encountered an unknown error processing your request.';
   return (
     <Container>
       <Box display="flex" justifyContent="center" alignItems="center">
         <img src={ErrorImage} alt="404" />
       </Box>
-      <Typography variant="h4" align="center">
-        This Page is Lost in the Wind
+      <Typography variant="h3" align="center">
+        {errorTitle}
       </Typography>
-      <Box display="flex" justifyContent="center">
-        <Button variant="text" href="/">
+      <Typography variant="h5" align="center" marginTop={2}>
+        {errorText}
+      </Typography>
+      <Box display="flex" justifyContent="center" marginTop={6}>
+        <Button variant="text" href="/" size={'large'}>
           Back to Home Page
         </Button>
       </Box>
