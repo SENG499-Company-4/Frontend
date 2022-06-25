c37bd33cea79059360ea24baca6c8c2571146725
Sam Warren <samwarren4@gmail.com>
2022-06-24 21:19:45 -0700

    rebase

--- app/src/components/pages/ScheduleTimetable.tsx
+++ app/src/components/pages/ScheduleTimetable.tsx
@@ -4,14 +4,26 @@ import 'devextreme/dist/css/dx.light.css';
 import Appointment from 'components/organisms/Appointment';
 import classData from 'data/clean.json';
 import { parseCalendarCourse, parseCalendarTeacher } from 'utils/utils';
+import { parseCalendarJSON } from 'utils/utils';
+import { useLocation } from 'react-router-dom';
+import { ICourse } from 'components/shared/interfaces/timetable.interfaces';
 
 //The current date will be +1 month in the UI, ex: 2021/Dec/10 -> 2022/Jan/10
 const currentDate = new Date(2021, 12, 10);
+
+interface IStateProps {
+  course: ICourse;
+}
 
 function ScheduleTimetable() {
   const calendarCourseData = parseCalendarCourse(JSON.parse(JSON.stringify(classData)));
   const calendarTeacherData = parseCalendarTeacher(JSON.parse(JSON.stringify(classData)));
 
+  const { state } = useLocation();
+  const { course } = state as IStateProps;
+  console.log('Got course: ', course);
+
+  const calendarData = parseCalendarJSON(JSON.parse(JSON.stringify(classData)));
   return (
     <>
       {/*@ts-ignore*/}
