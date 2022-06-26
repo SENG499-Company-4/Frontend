bdc5b566647f8a5fc90050d0851fa72d569a95cb
Ben Mazerolle <ben@Bens-MacBook-Pro.local>
2022-06-25 20:10:12 -0700

    added endpoint infrastructure and login

--- app/src/components/pages/Survey.tsx
+++ app/src/components/pages/Survey.tsx
@@ -3,22 +3,8 @@ import SurveyForm from 'components/organisms/SurveyForm';
 import Typography from '@mui/material/Typography';
 import ClassData from 'data/clean.json';
 import { Box } from '@mui/system';
-import { useQuery } from '@apollo/client';
-import { GET_USER } from 'components/shared/api/Queries';
 
 function Survey() {
-  const { loading, error, data } = useQuery(GET_USER, {
-    variables: { id: 1 }
-  });
-
-  if (loading) {
-    console.log('Loading');
-  } else if (error) {
-    console.log('error!');
-  } else {
-    console.log(data);
-  }
-
   return (
     <Box marginTop={2} display={'flex'} flexDirection={'column'} alignItems={'center'}>
       <Typography variant="h4" gutterBottom marginY={4}>
