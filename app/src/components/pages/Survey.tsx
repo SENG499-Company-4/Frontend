58a07107ae73f0e3d8fd8b9186122381e88a71e9
Ben Mazerolle <ben@Bens-MacBook-Pro.local>
2022-06-25 20:05:01 -0700

    added endpoint infrastructure and login

--- app/src/components/pages/Survey.tsx
+++ app/src/components/pages/Survey.tsx
@@ -3,8 +3,23 @@ import SurveyForm from 'components/organisms/SurveyForm';
 import Typography from '@mui/material/Typography';
 import ClassData from 'data/clean.json';
 import { Box } from '@mui/system';
+import { useQuery } from "@apollo/client";
+import { GET_USER } from 'components/shared/api/Queries';
 
 function Survey() {
+
+  const { loading, error, data } = useQuery(GET_USER, {
+    variables: { id: 1 },
+  });
+
+  if (loading) {
+    console.log("Loading");
+  } else if (error) { 
+    console.log("error!");
+  } else {
+    console.log(data);
+  }
+  
   return (
     <Box marginTop={2} display={'flex'} flexDirection={'column'} alignItems={'center'}>
       <Typography variant="h4" gutterBottom marginY={4}>
@@ -17,3 +32,4 @@ function Survey() {
 }
 
 export default Survey;
+
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
 import reportWebVitals from 'reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: 'https://seng499company4backend.herokuapp.com/',
+  cache: new InMemoryCache()
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
