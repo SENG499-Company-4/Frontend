ee078d7970c67244b9f2be87805e6f733258d1c1
Sam Warren <samwarren4@gmail.com>
2022-07-06 12:10:07 -0700

    rebase

--- app/src/components/pages/Schedule.tsx
+++ app/src/components/pages/Schedule.tsx
@@ -1,23 +1,86 @@
-import React from 'react';
+import React, { ReactNode, useEffect, useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { GET_SCHEDULE } from 'components/shared/api/Queries';
-import LoadingSpinner from 'components/organisms/LoadingSpinner';
-import ErrorPage from './ErrorPage';
+import { Box, Button, Chip, Typography } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { IScheduleListItem } from 'components/shared/interfaces/timetable.interfaces';
+import classData from 'data/clean.json';
+import { parseScheduleListItems } from 'utils/utils';
+import { useNavigate } from 'react-router-dom';
 
 function Schedule() {
+  const allData: IScheduleListItem[] = parseScheduleListItems(JSON.parse(JSON.stringify(classData)));
+  const [rows] = useState<IScheduleListItem[]>(allData);
+  const navigate = useNavigate();
+
   const { loading, error, data } = useQuery(GET_SCHEDULE, {
     variables: {
       year: 2022
     }
   });
 
-  if (!data || loading) {
-    return <LoadingSpinner isLoading={true} />;
-  } else if (error) {
-    return <ErrorPage code={'400'} message="Schedule retrieval failed. Please try again." />;
+  useEffect(() => {
+    console.log('Got information from GQL: ', data);
+  }, [loading, data, error]);
+
+  function parseDaysOfWeek(daysOfWeek: string[]): ReactNode {
+    const daysAbbreviations = ['M', 'T', 'W', 'R', 'F'];
+    return daysAbbreviations.map((day, index) => {
+      if (daysOfWeek.includes(day)) {
+        return <Chip sx={{ marginX: '2px' }} color="primary" key={index} label={day} />;
+      }
+      return <Chip sx={{ marginX: '2px' }} key={index} label={day} />;
+    });
   }
-  console.log(data);
-  return <div>{data.schedule}</div>;
+
+  const columns: GridColDef[] = [
+    { field: 'title', headerName: 'Course Title', width: 130 },
+    { field: 'startDate', headerName: 'Start Date', width: 120 },
+    { field: 'endDate', headerName: 'End Date', width: 120 },
+    { field: 'timeOfDay', headerName: 'Time of Day', width: 160 },
+    {
+      field: 'daysOffered',
+      headerName: 'Days of Week',
+      width: 220,
+      renderCell: (params) => {
+        return parseDaysOfWeek(params.value);
+      }
+    },
+    {
+      field: 'professors',
+      headerName: 'Professor',
+      width: 180,
+      renderCell: (params) => {
+        return (
+          <Button
+            onClick={() => {
+              navigate('/professors/' + params.value[0].id);
+            }}
+          >
+            {params.value[0].username}
+          </Button>
+        );
+      }
+    }
+  ];
+
+  return (
+    <Box sx={{ width: '60%', margin: 'auto' }}>
+      <Typography marginTop={5} marginBottom={2} variant="h4" sx={{ textAlign: 'center' }}>
+        Current Schedule
+      </Typography>
+      <DataGrid
+        getRowId={(row: IScheduleListItem) => {
+          return row.professors[0].id + row.title + row.capacity + row.courseNumber;
+        }}
+        autoHeight
+        rows={rows}
+        columns={columns}
+        pageSize={20}
+        rowsPerPageOptions={[5]}
+      />
+    </Box>
+  );
 }
 
 export default Schedule;
