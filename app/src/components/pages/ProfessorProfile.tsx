a5dddd77dfffe577aeed101a77978dd753383dc0
Sam Warren <samwarren4@gmail.com>
2022-07-11 01:37:05 -0700

    professor profile loader

--- app/src/components/pages/ProfessorProfile.tsx
+++ app/src/components/pages/ProfessorProfile.tsx
@@ -1,53 +1,60 @@
 import { BubbleChart, Code, DeveloperBoard } from '@mui/icons-material';
-import { Avatar, ButtonBase, Card, Grid, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material';
-import { Faculty, Role } from 'constants/timetable.constants';
-import { ICourse, IProfessor, IProfessorPreference } from 'interfaces/timetable.interfaces';
-import React from 'react';
+import {
+  Avatar,
+  ButtonBase,
+  Card,
+  CircularProgress,
+  Grid,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography
+} from '@mui/material';
+import { Faculty } from 'constants/timetable.constants';
+import { ICourse, IProfessorPreference } from 'interfaces/timetable.interfaces';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getCoursesForProfessor } from 'utils/utils';
-import classData from 'data/clean.json';
 import { useQuery } from '@apollo/client';
-import { GET_ME, GET_USER_BY_ID } from 'api/Queries';
+import { GET_USER_BY_ID } from 'api/Queries';
+import { LoadingContext } from 'contexts/LoadingContext';
+import { ErrorContext } from 'contexts/ErrorContext';
+import { CoursePreference, User } from 'types/api.types';
 
 function ProfessorProfile() {
   const navigate = useNavigate();
+  const loadingContext = useContext(LoadingContext);
+  const errorContext = useContext(ErrorContext);
 
+  const [professor, setProfessor] = useState<User>();
   const { id } = useParams();
-  console.log('Visiting profile of ' + id);
+  const paramId = id ? id : '-1';
 
-  // TBD: When backend completes the User object, grab user by ID and populate the page with data from API.
-  // Currently this code does nothing because it returns undefined.
-  const { loading: meLoading, error: meError, data: meData } = useQuery(GET_ME);
   const {
     loading: userLoading,
     error: userError,
     data: userData
   } = useQuery(GET_USER_BY_ID, {
     variables: {
-      id: id
+      id: parseInt(paramId)
     }
   });
 
-  console.log('ME DATA: ', meData, meLoading, meError);
-  console.log('USER DATA: ', userData, userLoading, userError);
+  useEffect(() => {
+    loadingContext.setLoading(userLoading);
+    if (userData) {
+      console.log('User data: ', userData);
+      setProfessor(userData.findUserById);
+      // setUserData(userData.findUserById);
+    }
+    if (userError) {
+      errorContext.setErrorDialog(userError);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userData, userError, userLoading]);
 
-  const professor: IProfessor = {
-    active: true,
-    id: 123,
-    role: Role.User,
-    username: 'hausi',
-    preferences: [
-      { id: { subject: 'SENG', code: '265', term: 'SPRING' }, preference: 172 },
-      { id: { subject: 'SENG', code: '480D', term: 'SPRING' }, preference: 150 },
-      { id: { subject: 'SENG', code: '265', term: 'SUMMER' }, preference: 133 }
-    ],
-    faculty: 'SENG'
-  };
-
-  const currentlyTeaching: ICourse[] = getCoursesForProfessor(
-    professor.username,
-    JSON.parse(JSON.stringify(classData))
-  );
+  // TODO: Could implement this later when we get courses
+  const currentlyTeaching: ICourse[] = getCoursesForProfessor(professor?.id);
 
   const facultyIcons = {
     SENG: <Code />,
@@ -79,92 +86,102 @@ function ProfessorProfile() {
 
   return (
     <Grid display="flex" marginTop={4} justifyContent="center" sx={{ width: '100%' }}>
-      <Card elevation={10} sx={{ width: '70%' }}>
-        <Grid display={'flex'} flexDirection={'row'} margin={4}>
-          <Grid item mr={2}>
-            <Avatar sx={{ bgcolor: '#3f80b5' }} />
+      <Card elevation={10} sx={{ width: '70%', minHeight: '400px' }}>
+        {userLoading ? (
+          <Grid display={'flex'} justifyContent={'center'} alignContent={'center'}>
+            <CircularProgress />
           </Grid>
-          <Grid item>
-            <Typography variant="h4">
-              <b>{professor.username}</b>
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid display={'flex'} flexDirection={'row'} margin={4}>
-          <Grid item>
-            <Typography variant="body1" color={professor.active ? 'green' : 'red'}>
-              <b>{professor.active ? 'Active' : 'Inactive'}</b>
-            </Typography>
-            <Typography variant="body1">
-              <b>Username: </b> {professor.username}
-            </Typography>
-            <Typography variant="body1">
-              <b>Faculty: </b> {professor.faculty}
-            </Typography>
-            <Typography variant="body1">
-              <b>Role: </b> {professor.role}
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid display={'flex'} flexDirection={'column'} margin={4}>
-          <Grid item>
-            <Typography variant="h5" marginBottom={2}>
-              <b>Preferences</b>
-            </Typography>
-          </Grid>
-          {professor.preferences.length > 0 ? (
-            professor.preferences?.map((preference: IProfessorPreference) => (
+        ) : (
+          <>
+            <Grid display={'flex'} flexDirection={'row'} margin={4}>
+              <Grid item mr={2}>
+                <Avatar sx={{ bgcolor: '#3f80b5' }} />
+              </Grid>
               <Grid item>
-                <Typography variant="body1">
-                  <b>{preference.id.subject + ' ' + preference.id.code}</b>: {getWilling(preference.preference)}
+                <Typography variant="h4">
+                  <b>{professor?.name}</b>
                 </Typography>
               </Grid>
-            ))
-          ) : (
-            <Typography variant="body1">No preferences recorded.</Typography>
-          )}
-        </Grid>
-        <Grid display={'flex'} flexDirection={'column'} margin={4}>
-          <Grid item>
-            <Typography variant="h5">
-              <b>Currently Teaching</b>
-            </Typography>
-          </Grid>
-          {currentlyTeaching.length > 0 ? (
-            <Grid container spacing={2}>
-              {currentlyTeaching.map((course: ICourse) => (
-                <Grid item marginY={1}>
-                  <ButtonBase
-                    onClick={() => {
-                      navigate('/schedule/timetable', {
-                        state: {
-                          courseId: course.CourseID.subject + course.CourseID.code
-                        }
-                      });
-                    }}
-                    sx={{ display: 'block', textAlign: 'initial' }}
-                  >
-                    <Card elevation={3} sx={{ width: '300px' }}>
-                      <ListItem>
-                        <ListItemAvatar>
-                          <Avatar sx={{ bgcolor: facultyColors[course.CourseID.subject as Faculty] }}>
-                            {facultyIcons[course.CourseID.subject as Faculty]}
-                          </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText
-                          primary={course.CourseID.subject + ' ' + course.CourseID.code}
-                          secondary={course.startDate + ' - ' + course.endDate}
-                        />
-                      </ListItem>
-                    </Card>
-                  </ButtonBase>
-                </Grid>
-              ))}
             </Grid>
-          ) : (
-            <Typography variant="body1">This professor is currently not teaching a course.</Typography>
-          )}
-        </Grid>
+            <Grid display={'flex'} flexDirection={'row'} margin={4}>
+              <Grid item>
+                <Typography variant="body1" color={professor?.active ? 'green' : 'red'}>
+                  <b>{professor?.active ? 'Active' : 'Inactive'}</b>
+                </Typography>
+                <Typography variant="body1">
+                  <b>Username: </b> {professor?.username}
+                </Typography>
+                {/* 
+            TODO: Implement this later when backend returns it
+            <Typography variant="body1">
+              <b>Faculty: </b> {professor?.faculty}
+            </Typography> */}
+                <Typography variant="body1">
+                  <b>Role: </b> {professor?.role}
+                </Typography>
+              </Grid>
+            </Grid>
+            <Grid display={'flex'} flexDirection={'column'} margin={4}>
+              <Grid item>
+                <Typography variant="h5" marginBottom={2}>
+                  <b>Preferences</b>
+                </Typography>
+              </Grid>
+              {professor && professor?.preferences && professor?.preferences?.length > 0 ? (
+                professor.preferences?.map((preference: CoursePreference) => (
+                  <Grid item>
+                    <Typography variant="body1">
+                      <b>{preference.id.subject + ' ' + preference.id.code}</b>: {getWilling(preference.preference)}
+                    </Typography>
+                  </Grid>
+                ))
+              ) : (
+                <Typography variant="body1">No preferences recorded.</Typography>
+              )}
+            </Grid>
+            <Grid display={'flex'} flexDirection={'column'} margin={4}>
+              <Grid item>
+                <Typography variant="h5">
+                  <b>Currently Teaching</b>
+                </Typography>
+              </Grid>
+              {currentlyTeaching.length > 0 ? (
+                <Grid container spacing={2}>
+                  {currentlyTeaching.map((course: ICourse) => (
+                    <Grid item marginY={1}>
+                      <ButtonBase
+                        onClick={() => {
+                          navigate('/schedule/timetable', {
+                            state: {
+                              courseId: course.CourseID.subject + course.CourseID.code
+                            }
+                          });
+                        }}
+                        sx={{ display: 'block', textAlign: 'initial' }}
+                      >
+                        <Card elevation={3} sx={{ width: '300px' }}>
+                          <ListItem>
+                            <ListItemAvatar>
+                              <Avatar sx={{ bgcolor: facultyColors[course.CourseID.subject as Faculty] }}>
+                                {facultyIcons[course.CourseID.subject as Faculty]}
+                              </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText
+                              primary={course.CourseID.subject + ' ' + course.CourseID.code}
+                              secondary={course.startDate + ' - ' + course.endDate}
+                            />
+                          </ListItem>
+                        </Card>
+                      </ButtonBase>
+                    </Grid>
+                  ))}
+                </Grid>
+              ) : (
+                <Typography variant="body1">This professor is currently not teaching a course.</Typography>
+              )}
+            </Grid>
+          </>
+        )}
       </Card>
     </Grid>
   );
