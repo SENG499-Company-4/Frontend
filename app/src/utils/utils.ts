e5763238d9ffff0c7321dbe7773c15608570586c
Sam Warren <samwarren4@gmail.com>
2022-07-11 01:37:23 -0700

    get util working with api changes

--- app/src/utils/utils.ts
+++ app/src/utils/utils.ts
@@ -111,12 +111,15 @@ export function getTeacherById(id: number) {
 }
 
 // Given a data source and a professor username, return courses that professor is teaching or has taught.
-export function getCoursesForProfessor(username: string, data: ICourse[]): ICourse[] {
+export function getCoursesForProfessor(id?: number, data?: ICourse[]): ICourse[] {
+  if (!id || !data) {
+    return [];
+  }
   const today = new Date();
   const courses: ICourse[] = [];
   data.forEach((course: ICourse) => {
     course.professors.forEach((professor) => {
-      if (professor.username === username) {
+      if (professor.id === id) {
         if (new Date(course.startDate) <= today && new Date(course.endDate) >= today) {
           courses.push(course);
         }
