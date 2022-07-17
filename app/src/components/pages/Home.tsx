a199c97e7fd6fb3065fa889576b4c85dc9c2d231
NZ369 <NZ369@users.noreply.github.com>
2022-07-17 04:12:23 +0000

    Prettified Code!

--- app/src/components/pages/Home.tsx
+++ app/src/components/pages/Home.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
 //import Grid from '@mui/material/Grid';
 //import { Typography } from '@mui/material';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import ImageSlider from 'components/organisms/ImageSlider'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import ImageSlider from 'components/organisms/ImageSlider';
 
-const images = [{
-  id: 1,
-  src: "https://iili.io/wOh3Nt.png",
-  alt: "Image 1"
-},
-{
-  id: 2,
-  src: "https://iili.io/wOWJqv.png",
-  alt: "Image 2 "
-},
-{
-  id: 3,
-  src: "https://iili.io/wOWPTX.png",
-  alt: "Image 3"
-}
+const images = [
+  {
+    id: 1,
+    src: 'https://iili.io/wOh3Nt.png',
+    alt: 'Image 1'
+  },
+  {
+    id: 2,
+    src: 'https://iili.io/wOWJqv.png',
+    alt: 'Image 2 '
+  },
+  {
+    id: 3,
+    src: 'https://iili.io/wOWPTX.png',
+    alt: 'Image 3'
+  }
 ];
 
 function Home() {
