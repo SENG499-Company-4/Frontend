d7ed474925d6632a339c1a20cad18f37f14c5e58
Sam Warren <samwarren4@gmail.com>
2022-07-20 18:06:16 -0700

    update appointment to get user info from db instead of dummy data

--- app/src/components/organisms/Appointment.tsx
+++ app/src/components/organisms/Appointment.tsx
@@ -1,27 +1,58 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { formatDate } from 'devextreme/localization';
-import { ICalendarItem_Teacher } from 'interfaces/timetable.interfaces';
-import { Grid } from '@mui/material';
+import { CircularProgress, Grid } from '@mui/material';
 import { Container } from '@mui/system';
-import { getTeacherById } from 'utils/utils';
+import { useQuery } from '@apollo/client';
+import { gql } from '@apollo/client';
 
 /**
  * Present data in following format: coursename(subject & code) + teacher + time
  */
 function Appointment(model: any) {
   const { targetedAppointmentData } = model.data;
-  const teacherInfo: ICalendarItem_Teacher = getTeacherById(targetedAppointmentData.teacherId) || {}; //link the teacher and course by teacherId
+
+  const GET_USER_BY_ID_LEAN = gql`
+    query FindUserById($findUserByIdId: Int!) {
+      findUserById(id: $findUserByIdId) {
+        username
+        id
+        name
+      }
+    }
+  `;
+
+  const {
+    data: getUserData,
+    loading: getUserLoading,
+    error: getUserError
+  } = useQuery(GET_USER_BY_ID_LEAN, {
+    variables: {
+      id: targetedAppointmentData.teacherId
+    }
+  });
+
+  useEffect(() => {
+    if (getUserData) {
+      console.log('Got user!', getUserData);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [getUserData, getUserLoading, getUserError]);
 
   return (
     <Container sx={{ height: '100%' }}>
       <Grid container className="showtime-preview" direction="column">
-        <Grid item> {targetedAppointmentData.courseId} </Grid>
-        <Grid item> {teacherInfo.teacherName} </Grid>
-        <Grid item>
-          {formatDate(targetedAppointmentData.displayStartDate, 'shortTime')}
-          {' - '}
-          {formatDate(targetedAppointmentData.displayEndDate, 'shortTime')}
-        </Grid>
+        {getUserLoading ? (
+          <CircularProgress />
+        ) : (
+          <>
+            <Grid item> {targetedAppointmentData.courseId} </Grid>
+            <Grid item> {getUserData.findUserById.name}</Grid>
+            <Grid item>
+              {formatDate(targetedAppointmentData.displayStartDate, 'shortTime')}
+              {formatDate(targetedAppointmentData.displayEndDate, 'shortTime')}
+            </Grid>
+          </>
+        )}
       </Grid>
     </Container>
   );
