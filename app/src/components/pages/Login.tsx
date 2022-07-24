f97cb919819f0f08c2b78a40caeefea83a9a312d
Sam Warren <samwarren4@gmail.com>
2022-07-23 19:08:21 -0700

    login styling

--- app/src/components/pages/Login.tsx
+++ app/src/components/pages/Login.tsx
@@ -88,83 +88,103 @@ const Login = () => {
   }, [formState]);
 
   return (
-    <Box component="form" sx={{ width: 300 }} mx="auto" justifyContent="center" noValidate autoComplete="off">
-      <Grid container spacing={2} className="login">
-        <Link to="/">
-          <img
-            style={{ width: 300, marginTop: 60 }}
-            className="login_logo"
-            src="https://web.uvic.ca/~virsf/graphics/UVic%20Logo.png"
-            alt=""
-          />
-        </Link>
-        <Grid item>
-          <Typography variant="h3">Sign In</Typography>
-        </Grid>
-        {hasErrors && (
-          <Grid item>
-            <Typography variant="body1" color="error">
-              {errorMessage}
-            </Typography>
+    <Grid container mx="auto" display={'flex'} justifyContent={'center'} alignItems={'center'}>
+      <Card
+        elevation={10}
+        style={{
+          paddingTop: '70px',
+          paddingBottom: '70px',
+          paddingLeft: '33px',
+          paddingRight: '33px',
+          width: '400px',
+          marginTop: '100px'
+        }}
+      >
+        <Grid
+          container
+          spacing={2}
+          className="login"
+          display={'flex'}
+          flexDirection={'column'}
+          justifyContent={'center'}
+          alignItems={'center'}
+        >
+          <Link to="/">
+            <img
+              style={{ width: 300 }}
+              className="login_logo"
+              src="https://web.uvic.ca/~virsf/graphics/UVic%20Logo.png"
+              alt=""
+            />
+          </Link>
+          <Grid item mb={2}>
+            <Typography variant="h3">Sign In</Typography>
           </Grid>
-        )}
-        <Grid item>
-          <TextField
-            id="outlined-username-input"
-            style={{ width: 300 }}
-            label="Username"
-            value={formState.username}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                login({ variables: { username: formState.username, password: formState.password } });
-              }
-            }}
-            onChange={(e) =>
-              setFormState({
-                ...formState,
-                username: e.target.value
-              })
-            }
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            id="outlined-password-input"
-            style={{ width: 300 }}
-            label="Password"
-            type="password"
-            value={formState.password}
-            onKeyDown={(e) => {
-              if (e.key === 'Enter') {
-                login({ variables: { username: formState.username, password: formState.password } });
-              }
-            }}
-            onChange={(e) =>
-              setFormState({
-                ...formState,
-                password: e.target.value
-              })
-            }
-          />
-        </Grid>
-        <Grid item>
-          <Button
-            variant="contained"
-            style={{ width: 300, height: 56 }}
-            onClick={() =>
-              login({
-                variables: {
-                  username: formState.username,
-                  password: formState.password
+          {hasErrors && (
+            <Grid item>
+              <Typography variant="body1" color="error">
+                {errorMessage}
+              </Typography>
+            </Grid>
+          )}
+          <Grid item>
+            <TextField
+              id="outlined-username-input"
+              style={{ width: 300 }}
+              label="Username"
+              value={formState.username}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  login({ variables: { username: formState.username, password: formState.password } });
                 }
-              })
-            }
-          >
-            Sign In
-          </Button>
+              }}
+              onChange={(e) =>
+                setFormState({
+                  ...formState,
+                  username: e.target.value
+                })
+              }
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="outlined-password-input"
+              style={{ width: 300 }}
+              label="Password"
+              type="password"
+              value={formState.password}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  login({ variables: { username: formState.username, password: formState.password } });
+                }
+              }}
+              onChange={(e) =>
+                setFormState({
+                  ...formState,
+                  password: e.target.value
+                })
+              }
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              style={{ width: 300, height: 56 }}
+              onClick={() =>
+                login({
+                  variables: {
+                    username: formState.username,
+                    password: formState.password
+                  }
+                })
+              }
+            >
+              Sign In
+            </Button>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
+      </Card>
+    </Grid>
   );
 };
 
