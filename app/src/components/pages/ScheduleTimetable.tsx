f17c81c54d6300aaad8802cfe25c15bf5ed862b6
Ahnaf-Ahmed <Ahnaf-Ahmed@users.noreply.github.com>
2022-07-26 00:52:56 +0000

    Prettified Code!

--- app/src/components/pages/ScheduleTimetable.tsx
+++ app/src/components/pages/ScheduleTimetable.tsx
@@ -374,28 +374,28 @@ function ScheduleTimetable() {
           <DialogContentText id="alert-dialog-slide-description">
             {Object.keys(profErrorIndex).length > 0
               ? Object.values(profErrorIndex).map((prof: IProfessorIndexEntry) => {
-                return (
-                  <>
-                    <Typography>{prof.username + ' has course overlaps'}</Typography>
-                    {prof.classes.map((course) => {
-                      return (
-                        <Typography>
-                          {/* this line is and the one below is is purely for adding a new line for readability and it doesnt seem to process a "\n" */}
-                          <Typography>{'-'}</Typography>
-                          {course.courseId + ' Section ' + course.capacity}
+                  return (
+                    <>
+                      <Typography>{prof.username + ' has course overlaps'}</Typography>
+                      {prof.classes.map((course) => {
+                        return (
                           <Typography>
-                            {course.meetingTime.day +
-                              ' ' +
-                              course.meetingTime.startTime.split('T')[0] +
-                              '-' +
-                              course.meetingTime.endTime.split('T')[0]}
+                            {/* this line is and the one below is is purely for adding a new line for readability and it doesnt seem to process a "\n" */}
+                            <Typography>{'-'}</Typography>
+                            {course.courseId + ' Section ' + course.capacity}
+                            <Typography>
+                              {course.meetingTime.day +
+                                ' ' +
+                                course.meetingTime.startTime.split('T')[0] +
+                                '-' +
+                                course.meetingTime.endTime.split('T')[0]}
+                            </Typography>
                           </Typography>
-                        </Typography>
-                      );
-                    })}
-                  </>
-                );
-              })
+                        );
+                      })}
+                    </>
+                  );
+                })
               : "Your re-generation request was submitted successfully. When the scheduling algorithm checks complete, you'll be able to view the updated schedule here."}
           </DialogContentText>
         </DialogContent>
